051edd4c4d92b8c8dafb8db405da2a45277302e6
maksimsavi <savianokm@gmail.com>
2020-12-14 13:33:25 -0600

    fixed auth and logger

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const bookmarkRouter = require('./bookmark/bookmark-router')
 const listRouter = require('./list/list-router')
+const winston = require('winston');
 
 const app = express()
 
@@ -14,12 +15,27 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+//logger
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.File({ filename: 'info.log' })
+    ]
+  });
+  
+  if (NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple()
+    }));
+  }
+
 //token validation
 app.use(function validateBearerToken(req,res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken  = req.get("Authorization")
     
-    if (authToken !== apiToken) {
+    if (authToken.split(' ')[1] !== apiToken) {
         logger.error('Unauthorized request to: '+req.path)
         console.log(authToken+" "+apiToken)
         return res.status(401).json({ error: "Unauthorized request"})
